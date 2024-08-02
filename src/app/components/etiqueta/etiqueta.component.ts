32a1083dca9adc53e90ee5884cf1eae00e7fdfd4
AngelBeltranM <100233415+AngelBeltranM@users.noreply.github.com>
2024-08-02 14:15:18 -0600

    cambio de codigo de barras a qr

--- src/app/components/etiqueta/etiqueta.component.ts
+++ src/app/components/etiqueta/etiqueta.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import JsBarcode from 'jsbarcode';
+import QRCode from 'qrcode';
 
 
 @Component({
@@ -9,73 +11,76 @@ import JsBarcode from 'jsbarcode';
 })
 export class EtiquetaComponent {
 
-  showLabel = false;
+  formulario: FormGroup;
+  barcodeData: string = '';
 
-  generateLabel(event: Event) {
-    event.preventDefault();
+  qrCodeDataUrl: string = '';
 
-    const numeroUnicoInput = document.getElementById('numeroUnico') as HTMLInputElement;
-    const equipmentTypeSelect = document.getElementById('equipmentType') as HTMLSelectElement;
-    const nombreEquipoInput = document.getElementById('nombreEquipo') as HTMLInputElement;
-    const numeroSerieInput = document.getElementById('numeroSerie') as HTMLInputElement;
-    const purchaseDateInput = document.getElementById('purchaseDate') as HTMLInputElement;
 
-    if (!numeroUnicoInput || !equipmentTypeSelect || !nombreEquipoInput || !numeroSerieInput || !purchaseDateInput) {
-      console.error('Uno o más elementos del formulario no se encontraron.');
-      return;
-    }
-
-    const numeroUnico = numeroUnicoInput.value;
-    const equipmentType = equipmentTypeSelect.value;
-    const nombreEquipo = nombreEquipoInput.value;
-    const numeroSerie = numeroSerieInput.value;
-    const purchaseDate = purchaseDateInput.value;
-
-    const labelContent = `Número Único: ${numeroUnico}\nTipo: ${equipmentType}\nNombre: ${nombreEquipo}\nSerie: ${numeroSerie}\nFecha: ${purchaseDate}`;
-
-  
-    // Generar código de barras
-    const barcodeCanvas = document.getElementById('barcode') as HTMLCanvasElement;
-    if (barcodeCanvas) {
-      JsBarcode(barcodeCanvas, numeroUnico, {
-        format: 'CODE128',
-        displayValue: true,
-        fontSize: 18
-      });
-    } else {
-      console.error('Elemento barcode no encontrado.');
-    }
-
-  
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      NoUnico: [''],
+      equimentType: [''],
+      nombreEquipo: [''],
+      NoSerie: [''],
+      Date: ['']
+    });
   }
 
+  generarCodigoDeBarras() {
 
-  printBarcode() {
-    const barcodeCanvas = document.getElementById('barcode') as HTMLCanvasElement;
-    if (barcodeCanvas) {
-      const printWindow = window.open('', '', 'height=600,width=800');
-      if (printWindow) {
-        printWindow.document.write('<html><head><title>Imprimir Código de Barras</title>');
-        printWindow.document.write('<style>');
-        printWindow.document.write('@page { size: A4 landscape; margin: 0; }');
-        printWindow.document.write('body { margin: 0; padding: 0; display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh; }');
-        printWindow.document.write('h1 { text-align: center; margin: 20px 0; }');
-        printWindow.document.write('img { display: block; width: 80%; max-width: 100%; margin: 0 auto; }');
-        printWindow.document.write('</style>');
-        printWindow.document.write('</head><body>');
-        printWindow.document.write('<h1>Código de Barras</h1>');
-        printWindow.document.write('<img src="' + barcodeCanvas.toDataURL() + '" />');
-        printWindow.document.write('</body></html>');
-        printWindow.document.close();
-        printWindow.focus();
-        printWindow.print();
-      } else {
-        console.error('No se pudo abrir la ventana de impresión.');
+    const NoUnico = this.formulario.get('NoUnico')?.value;
+    const equimentType = this.formulario.get('equimentType')?.value;
+    const nombreEquipo = this.formulario.get('nombreEquipo')?.value;
+    const NoSerie = this.formulario.get('NoSerie')?.value;
+    const Date = this.formulario.get('Date')?.value;
+
+
+    const datosConcatenados = `
+    {
+      ${NoUnico},
+      ${equimentType},
+      ${nombreEquipo},
+      ${NoSerie},
+      ${Date}
+    }`
+
+    QRCode.toDataURL(datosConcatenados, { width: 200, margin: 2 }, (err, url) => {
+      
+      if(err) {
+        console.error(err);
+        return;
       }
-    } else {
-      console.error('Elemento barcode no encontrado para impresión.');
+
+      this.qrCodeDataUrl = url;
+
+    });
+
+  }
+
+
+
+  //Metodo para imprimir el codigo de barras
+  printQRCode() {
+    const printWindow = window.open('', '', 'width=600,height=600');
+    if (printWindow) {
+      printWindow.document.open();
+      printWindow.document.write(`
+        <html>
+          <head>
+            <title>Print QR Code</title>
+          </head>
+          <body>
+            <img src="${this.qrCodeDataUrl}" />
+          </body>
+        </html>
+      `);
+      printWindow.document.close();
+      printWindow.focus();
+      printWindow.print();
     }
   }
+  
 
 
 }
